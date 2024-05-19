ae864dd30bd993b2266bced1866997fe71c622c3
Luka Šveigl <ls6727@hpc-login1.arnes.si>
2024-05-19 12:42:37 +0200

    Misc updates

--- src/sbatch_run_multi_task.sh
+++ src/sbatch_run_multi_task.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=nlp-baseline 
+#SBATCH --job-name=nlp-multi-task 
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1 #--gpus=1
-#SBATCH --output=baseline.out
+#SBATCH --output=multi-task.out
 #SBATCH --reservation=fri
 #SBATCH --time=04:00:00
-
-FILE=baseline.py
 
 module load CUDA/12.1.1
 
